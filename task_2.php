9217ee8bc91c63a36c098fef330ea06f894c0ca5
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-26 13:15:18 +0600

    Update task_2.php

--- task_2.php
+++ task_2.php
@@ -1,2 +1,12 @@
 <?php
-echo "Task-2";
+$numbers=[1,2,3,4,5,6,7,8,9,10];
+function removeEven($numbers){
+  if($numbers==0){
+    echo "Invalid Number";
+  }
+  return $numbers%2!=0;
+}
+$result=array_filter($numbers,"removeEven");
+foreach($result as $item){
+  echo $item.PHP_EOL;
+}
