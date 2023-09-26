9042b6342cfb1a6b43ad0fdf33aba6e969140da5
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-26 13:20:39 +0600

    Update task_3.php

--- task_3.php
+++ task_3.php
@@ -1,2 +1,9 @@
 <?php
-echo "Task-3";
+ $grades=[85, 92, 78, 88, 95];
+ function gradeSort($grades){
+   $result= rsort($grades);
+   foreach($grades as $item){
+     echo $item.PHP_EOL;
+   }
+ }
+ gradeSort($grades);
