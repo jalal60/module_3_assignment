604f7f85dbece1bb2aa4f6622da3b9ed985b4fb5
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-26 13:01:52 +0600

    Update task_1.php

--- task_1.php
+++ task_1.php
@@ -1,10 +1,10 @@
 <?php
-	$text="The quick brown fox jumps over the lazy dog.";
-  static $search = "brown";
-  static $replace="red";
-  function converter($text){
-    return strtolower($text);
-	}
-	$modifiedText=converter($text);
-	$result=str_replace($search,$replace,$modifiedText);
-	echo $result;
+$text="The quick brown fox jumps over the lazy dog.";
+static $search = "brown";
+static $replace="red";
+function converter($text){
+return strtolower($text);
+}
+$modifiedText=converter($text);
+$result=str_replace($search,$replace,$modifiedText);
+echo $result;
