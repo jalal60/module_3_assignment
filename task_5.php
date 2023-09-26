b22c46eb963897d5b8d32252070055072b7e59f5
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-26 21:09:50 +0600

    Update task_5.php

--- task_5.php
+++ task_5.php
@@ -4,5 +4,4 @@ function generatePassword($length)
     $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
     return substr(str_shuffle($chars), 0, $length);
 }
-
 echo generatePassword(12);
