63a5d43973fa6b5241254d4766d8593e79e1abe1
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-26 20:31:24 +0600

    Update task_4.php

--- task_4.php
+++ task_4.php
@@ -1,2 +1,19 @@
-<?php 
-echo "Task-4";
+<?php
+$studentGrades = [
+    "Student_1" => ["Math" => 80, "English" => 70, "Science" => 60],
+    "Student_2" => ["Math" => 70, "English" => 65, "Science" => 90],
+    "Student_3" => ["Math" => 60, "English" => 75, "Science" => 80]
+];
+
+function avgGrade($studentGrades)
+{
+    foreach ($studentGrades as $student => $subject) {
+        $avg = 0;
+        foreach ($subject as $key => $value) {
+            $avg += $value;
+        }
+        $result = $avg / 3;
+        echo "$student Average grade=$result\n";
+    }
+}
+avgGrade($studentGrades);
