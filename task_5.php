4065b74917866d0a7f91758ad9579166a6c0159c
Md. Shahjalal <jalal2k4@gmail.com>
2023-09-27 17:48:32 +0600

    Update task_5.php

--- task_5.php
+++ task_5.php
@@ -1,7 +1,20 @@
-<?php
-function generatePassword($length)
-{
-    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
-    return substr(str_shuffle($chars), 0, $length);
+<?php 
+function generatePassword($length) {
+
+    $_alphaSmall = 'abcdefghijklmnopqrstuvwxyz';           
+    $_alphaCaps  = strtoupper($_alphaSmall);               
+    $_numerics   = '1234567890';                           
+    $_specialChars = '!@#$%^&*()_+';   
+
+    $_container = $_alphaSmall.$_alphaCaps.$_numerics.$_specialChars;   
+    $password = '';       
+
+    for($i = 0; $i < $length; $i++) {                                 
+        $_rand = rand(0, strlen($_container) - 1);                
+        $password .= substr($_container, $_rand, 1);               
+    }
+
+    return $password;       // Returns the generated Pass
 }
+
 echo generatePassword(12);
